8675259c5d9b2f1e6ff5e89628d5d12dc8f7cdae
nataliesha <natalysa@gmail.com>
2021-10-24 11:25:46 +0300

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+       <div>
       <header className="App-header">
-        <h1>Welcom to my app!</h1>
-      </header>
+      <h1>Welcom to my app!</h1>
+            </header>
+      </div>
     </div>
   );
 }
