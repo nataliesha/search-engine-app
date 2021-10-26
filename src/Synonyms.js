81a291fac4e0a3887004f874dea001af8101a84c
nataliesha <natalysa@gmail.com>
2021-10-26 11:19:27 +0300

    Completed synonyms

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,5 +1,16 @@
 import react from "react";
 
 export default function Synonyms(props) {
-    
-}
+    if (props.synonyms) {
+        return (
+            <ul className="synonyms">
+            {props.synonyms.map(function(synonym, index) {
+                return <li key={index}>{synonym}</li>
+             } ) }
+   
+            </ul>
+            );
+        
+} else {
+    return null;
+}}
