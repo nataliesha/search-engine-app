e8c22328504abdbca83caac9e3f3bd8807ff61c8
nataliesha <natalysa@gmail.com>
2021-10-26 12:24:28 +0300

    Added an audio func for the phonetic

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,6 +1,18 @@
 import React from "react";
 
 
-export default function Phonetic() {
-    
+export default function Phonetic(props) {
+    let audio = new Audio(props.phonetic.audio)
+
+const start = () => {
+    audio.play()
+}
+
+    return (
+        <div className="phonetic">
+            <button className="btn btn-primary border-0" onClick={start}>▶︎ &nbsp; Listen</button>
+            <span className="pronounce">({props.phonetic.text})</span>
+        </div>
+    );
+
 }
