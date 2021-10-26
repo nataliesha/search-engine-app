419a2439ef4ceb11f1fee9e0b8f4ab256d88a00f
nataliesha <natalysa@gmail.com>
2021-10-26 10:35:45 +0300

    Added "You searched for:" and <em> the word

--- src/Results.js
+++ src/Results.js
@@ -5,7 +5,7 @@ export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <h2>{props.results.word}</h2>
+        <h2>You searched for: <em>{props.results.word}</em></h2>
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
