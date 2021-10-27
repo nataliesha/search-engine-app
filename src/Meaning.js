b33d4f3df773d7bfbb617c14e07583c08cd1d53a
nataliesha <natalysa@gmail.com>
2021-10-27 11:55:36 +0300

    Styling

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,20 +1,22 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import "./Meaning.css";
 
 export default function Meaning(props) {
   return (
-    <div className="Meaning">
+    <div className="meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-             <strong>Definition: </strong> {definition.definition}
-              <br />
-              <strong>Example: </strong><em>{definition.example}</em>
-              <br />
+            <div className="definition">
+              {definition.definition}
+              </div>
+              <div className="example">
+             {definition.example}
+             </div>
               <Synonyms synonyms={definition.synonyms} />
-            </p>
+            
           </div>
         );
       })}
